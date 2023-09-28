d79905bc5f3fe7f72feee9a6defd801c85df9725
Nacho <nacho.novello@gmail.com>
2023-09-28 18:49:47 -0300

    Corregí el middleware de auth http básica

--- Semana_06-Autenticacion_autorizacion_auditoria/01_auth_http/src/middlewares/httpAuthMiddleware.js
+++ Semana_06-Autenticacion_autorizacion_auditoria/01_auth_http/src/middlewares/httpAuthMiddleware.js
@@ -3,7 +3,10 @@ const service = require('../services/usersService');
 function httpAuthMiddleware(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        res.status(401).setHeader('WWW-Authenticate', 'Basic realm="Authentication required"').send('Unauthorized');
+        res
+            .status(401)
+            .setHeader('WWW-Authenticate', 'Basic realm="Authentication required"')
+            .send('Unauthorized');
         return;
     }
 
@@ -12,14 +15,17 @@ function httpAuthMiddleware(req, res, next) {
     const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
     const [username, password] = credentials.split(':');
 
-    const user = service.find(username, password);
-    if (!user) {
-        // Authentication failed
-        res.status(401).setHeader('WWW-Authenticate', 'Basic realm="Authentication required"').send('Unauthorized');
-        return;
-    }
+    service.find(username, password).then((user) => {
+        if (!user) {
+            // Authentication failed
+            res.status(401)
+                .setHeader('WWW-Authenticate', 'Basic realm="Authentication required"')
+                .send('Unauthorized');
+            return;
+        }
 
-    next();
+        next();
+    });
 }
 
 module.exports = { httpAuthMiddleware };
