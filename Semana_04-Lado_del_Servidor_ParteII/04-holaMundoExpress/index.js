1fb245ce27d58c186eafa9829ad7b59b0b0268a2
Nacho <nacho.novello@gmail.com>
2024-09-03 22:04:17 -0300

    2024-09-04 - Ejemplos

--- Semana_04-Lado_del_Servidor_ParteII/04-holaMundoExpress/index.js
+++ Semana_04-Lado_del_Servidor_ParteII/04-holaMundoExpress/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 //Rutas
 app.get('/', (req, res) => {
