ec966a9e28937a7bb90dc15eb261cb004492730f
Nacho <nacho.novello@gmail.com>
2024-09-03 22:03:14 -0300

    2024-09-04 - Ejemplos

--- Semana_04-Lado_del_Servidor_ParteII/01-holaMundo/01-holaMundo.js
+++ Semana_04-Lado_del_Servidor_ParteII/01-holaMundo/01-holaMundo.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+import http from 'http';
+const port = 3000;
 const server = http.createServer((req, res) => {
 	res.writeHead(200, { 'Content-Type': 'text/plain' })
 	res.end('Hola Mundo!')
