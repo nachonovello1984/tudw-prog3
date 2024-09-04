20c29d1d5df3f63538a008c29bd08e7c4e999fb3
Nacho <nacho.novello@gmail.com>
2024-09-04 17:41:38 -0300

    Clase 2024-09-04

---
+++ Semana_04-Lado_del_Servidor_ParteII/09-APIRest/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import fs from 'fs';
+
+const port = 3000;
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+
+    fs.readFile('./data/automoviles.json', (err, data) => {
+        if (err) {
+            console.log(err);
+            throw new Exception(err);
+        }
+
+        let automoviles = JSON.parse(data);
+
+        res.json(automoviles);
+    });
+});
+
+app.listen(port, () => console.log(`Servidor iniciado en el puerto ${port}`))
