30ffe063fd3973de976ae2a6d6dc3b9bbfd07b4a
Nacho <nacho.novello@gmail.com>
2024-10-17 16:20:48 -0300

    2024-10-17

--- APIs_Adicional/soap/index.js
+++ APIs_Adicional/soap/index.js
@@ -1,38 +1,9 @@
-const soap = require('soap');
+// import soap from 'soap';
 
-const url = 'http://localhost:8080/SoapJavaProject/HelloSoap?wsdl';
-
-const args = {
-  name: "Nacho Novello"
-};
-
-soap.createClient(url, (err, client) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  // Llama a una función del servicio web SOAP
-  client.hello(args, (err, result) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    console.log(result);
-  });
-});
-
-// const soap = require('soap');
-
-// const url = 'http://localhost:8080/SoapJavaProject/PersonasSoap?wsdl';
+// const url = 'http://localhost:8080/SoapJavaProject/HelloSoap?wsdl';
 
 // const args = {
-//   persona:{
-//     apellido: "Novello",
-//     nombre: "Ignacio",
-//     edad: 38
-//   }
+//   name: "Nacho Novello"
 // };
 
 // soap.createClient(url, (err, client) => {
@@ -42,7 +13,7 @@ soap.createClient(url, (err, client) => {
 //   }
 
 //   // Llama a una función del servicio web SOAP
-//   client.playback(args, (err, result) => {
+//   client.hello(args, (err, result) => {
 //     if (err) {
 //       console.error(err);
 //       return;
@@ -51,3 +22,36 @@ soap.createClient(url, (err, client) => {
 //     console.log(result);
 //   });
 // });
+
+// import soap from 'soap';
+
+// const url = 'http://localhost:8080/SoapJavaProject/WsPersonas?wsdl';
+
+// soap.createClient(url, (err, client) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+
+//   // Llama a una operación del servicio web SOAP
+//   client.findAll((err, result) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+
+//     console.log(result);
+//   });
+// });
+
+import soap from 'soap';
+
+try {
+  const url = 'http://localhost:8080/SoapJavaProject/WsPersonas?wsdl';
+  const args = null;
+  const client = await soap.createClientAsync(url);
+  const [result] = await client["findAllAsync"](args);
+  console.log(result);
+} catch (exc) {
+  console.log(err);
+}
