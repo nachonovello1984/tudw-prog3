9b2752b528e34a28a21a021c9742ff2b0592fc06
Nacho <nacho.novello@gmail.com>
2024-10-31 22:14:22 -0300

    2024-10-31

--- Semana_08_Arboles/index.js
+++ Semana_08_Arboles/index.js
@@ -1,46 +1,46 @@
-import BinarySearchTree from './binarySearchTree/binarySearchTree.js';
+// import BinarySearchTree from './binarySearchTree/binarySearchTree.js';
 import ChildrenListTree from "./childrenListTree/childrenListTree.js";
 import Node from "./childrenListTree/node.js";
 
-function bstClient() {
-    const arbol = new BinarySearchTree();
+// function bstClient() {
+//     const arbol = new BinarySearchTree();
 
-    arbol.insert(11);
-    arbol.insert(7);
-    arbol.insert(15);
-    arbol.insert(5);
-    arbol.insert(3);
-    arbol.insert(6)
-    arbol.insert(9);
-    arbol.insert(8);
-    arbol.insert(10);
-    arbol.insert(13);
-    arbol.insert(12);
-    arbol.insert(14);
-    arbol.insert(20);
-    arbol.insert(18);
-    arbol.insert(25);
+//     arbol.insert(11);
+//     arbol.insert(7);
+//     arbol.insert(15);
+//     arbol.insert(5);
+//     arbol.insert(3);
+//     arbol.insert(6)
+//     arbol.insert(9);
+//     arbol.insert(8);
+//     arbol.insert(10);
+//     arbol.insert(13);
+//     arbol.insert(12);
+//     arbol.insert(14);
+//     arbol.insert(20);
+//     arbol.insert(18);
+//     arbol.insert(25);
 
-    console.log(`arbol: ${arbol.toString()}`);
-    console.log(`arbol.min(): ${arbol.min().toString()}`);
-    console.log(`arbol.max(): ${arbol.max().toString()}`);
-    // arbol.remove(25);
-    // console.log(`arbol.remove(25): ${arbol.toString()}`);
-    // console.log(`arbol.max(): ${arbol.max().toString()}`);
-
-
-    console.log(`arbol.preOrderTraverse():`);
-    arbol.preOrderTraverse(x => process.stdout.write(`${x},`));
+//     console.log(`arbol: ${arbol.toString()}`);
+//     console.log(`arbol.min(): ${arbol.min().toString()}`);
+//     console.log(`arbol.max(): ${arbol.max().toString()}`);
+//     // arbol.remove(25);
+//     // console.log(`arbol.remove(25): ${arbol.toString()}`);
+//     // console.log(`arbol.max(): ${arbol.max().toString()}`);
 
 
+//     console.log(`arbol.preOrderTraverse():`);
+//     arbol.preOrderTraverse(x => process.stdout.write(`${x},`));
 
-    // console.log(`\narbol.inOrderTraverse():`);
-    // arbol.inOrderTraverse(x => process.stdout.write(`${x},`));
-    // console.log();
-}
+
+
+//     // console.log(`\narbol.inOrderTraverse():`);
+//     // arbol.inOrderTraverse(x => process.stdout.write(`${x},`));
+//     // console.log();
+// }
 
 function childrenListClient() {
-    const nodeProg = new Node("Programación");
+    const nodeProg  = new Node("Programación");
     const nodeProg1 = new Node("Programación 1");
     const nodeProg2 = new Node("Programación 2");
     const nodeIntroDevWeb = new Node("Introducción al Desarrollo Web");
@@ -68,5 +68,5 @@ function childrenListClient() {
     console.log(`arbol: ${arbol.toString()}`);
 }
 
-bstClient();
-//childrenListClient();
+//bstClient();
+childrenListClient();
